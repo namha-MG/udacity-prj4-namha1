caeb7395089d4596973cd4a92935ab97c2b650a5
namha-MG <t524082017@gmail.com>
2022-12-11 16:23:22 +0700

    Complete todo task + auth

--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -1,5 +1,5 @@
 import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
+const AWSXRay = require('aws-xray-sdk');
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { createLogger } from '../utils/logger'
 import { TodoItem } from '../models/TodoItem'
@@ -10,3 +10,111 @@ const XAWS = AWSXRay.captureAWS(AWS)
 const logger = createLogger('TodosAccess')
 
 // TODO: Implement the dataLayer logic
+export class TodosAccess {
+    constructor(
+        private readonly docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient(),
+        private readonly todosTable = process.env.TODOS_TABLE,
+        private readonly todoCreatedIndex = process.env.TODOS_CREATED_AT_INDEX
+    ) { }
+    async getAllTodos(userId: string): Promise<TodoItem[]> {
+        logger.info('Start getting all todos')
+
+        const result = await this.docClient.query({
+            TableName: this.todosTable,
+            IndexName: this.todoCreatedIndex,
+            KeyConditionExpression: 'userId = :userId',
+            ExpressionAttributeValues: {
+                ':userId': userId
+            }
+        }, (err) => {
+            if (err) {
+                logger.info('Failed to get todo items')
+                throw new Error("Failed to get todo items")
+            }
+        }).promise()
+
+        const items = result.Items
+        return items as TodoItem[]
+    }
+    async createTodoItem(todoItem: TodoItem): Promise<TodoItem> {
+        logger.info('Start create new todo item')
+
+        await this.docClient.put({
+            TableName: this.todosTable,
+            Item: todoItem
+        }, (err) => {
+            if (err) {
+                logger.info('Failed to create todo item')
+                throw new Error("Failed to create todo item")
+            }
+        }).promise()
+
+        return todoItem
+    }
+    async updateTodoItem(todoId: String, userId: String, updateTodoItem: TodoUpdate): Promise<TodoUpdate> {
+        logger.info('Start update todo item')
+
+        await this.docClient.update({
+            TableName: this.todosTable,
+            Key: {
+                todoId: todoId,
+                userId: userId
+            },
+            UpdateExpression: "set #name = :name, dueDate = :dueDate, done = :done",
+            ExpressionAttributeNames: {
+                '#name': 'name',
+            },
+            ExpressionAttributeValues: {
+                ":name": updateTodoItem.name,
+                ":dueDate": updateTodoItem.dueDate,
+                ":done": updateTodoItem.done
+            }
+        }, (err) => {
+            if (err) {
+                logger.info('Failed to update todo item')
+                throw new Error("Failed to update todo item")
+            }
+        }).promise()
+
+        return updateTodoItem
+    }
+    async deleteTodoItem(todoId: String, userId: String) {
+        logger.info('Start delete todo item')
+
+        await this.docClient.delete({
+            TableName: this.todosTable,
+            Key: {
+                todoId: todoId,
+                userId: userId
+            }
+        }, (err) => {
+            if (err) {
+                logger.info('Failed to delete todo item')
+                throw new Error("Failed to delete todo item")
+            }
+        }).promise()
+    }
+    async updateAttachmentUrl(todoId: string, userId: string, attachmentUrl: string): Promise<string> {
+        logger.info('Start update attachment url')
+
+        await this.docClient.update({
+            TableName: this.todosTable,
+            Key: {
+                todoId: todoId,
+                userId: userId
+            },
+            UpdateExpression: "set attachmentUrl = :attachmentUrl",
+            ExpressionAttributeValues: {
+                ":attachmentUrl": attachmentUrl,
+            }
+        }, (err) => {
+            if (err) {
+                logger.info('Failed to update attachment url')
+                throw new Error("Failed to update attachment url")
+            }
+        }).promise()
+
+        return attachmentUrl
+    }
+}
+
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = '6c4n8sqy7c'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-16cfurpbf6bjza6f.us.auth0.com',            // Auth0 domain
+  clientId: 'zqgb6d5vZiBu6vXspQ2Tcw1FBpbqZInN',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
